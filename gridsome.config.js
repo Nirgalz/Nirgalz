a38e0959ba614cc416380aa83cfe58e0a58f1f98
NH <znirgal@gmail.com>
2020-04-10 10:05:31 +0200

    hf

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Nicolas Hel - full stack developer',
+  siteDescription: 'website pro',
 
   templates: {
     Post: '/:title',
