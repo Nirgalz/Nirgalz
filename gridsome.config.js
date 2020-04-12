93f01ee439251a8a50b3a3b5ea3ee26009d96e7e
NH <znirgal@gmail.com>
2020-04-12 11:08:26 +0200

    added google analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -29,6 +29,12 @@ module.exports = {
           }
         }
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-30289272-4'
+      }
     }
   ],
 
